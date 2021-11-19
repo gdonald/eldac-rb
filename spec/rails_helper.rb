c54ade8f12beaf5ccc371ffe9402f67486f8de0e
Greg Donald <gdonald@gmail.com>
2021-11-19 14:05:41 -0600

    bundle update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -93,12 +93,12 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
+    "goog:chromeOptions": { args: %w[headless disable-gpu] }
   )
 
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
-                                 desired_capabilities: capabilities
+                                 capabilities: capabilities
 end
 
 Capybara.javascript_driver = if ENV['CHROME'] == 'true'
