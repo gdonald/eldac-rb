f5308451376519c7b3fdf19acd914177b364da7c
Greg Donald <gd@darkclear.net>
2023-10-09 12:02:28 -0500

    Fix zw

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,7 @@
-# rubocop:disable Style/FrozenStringLiteralComment
+# frozen_string_literal: true
 
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# rubocop:enable Style/FrozenStringLiteralComment
